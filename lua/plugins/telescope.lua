4bc26edbf496907a9c5bcdc76954c50933ce4d9f
ElWazy <santiago.fierro4@outlook.cl>
2025-11-14 14:02:54 -0300

    feat: better theme for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,10 @@ return {
   'nvim-telescope/telescope.nvim', tag = '0.1.9',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    require('telescope').setup{
+      defaults = require('telescope.themes').get_ivy {
+      }
+    }
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope live grep' })
